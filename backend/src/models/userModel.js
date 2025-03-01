97210e8e0d16f2085c7d03c8da2707792fe37f4d
augolv <soninclovis@gmail.com>
2025-03-01 19:03:12 -0300

    feat: implement userModel with create and find functions

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,19 @@
+import database from "../db.js";
+
+export async function createUser(user) {
+  const { name, email, hashPassword } = user;
+  const queryString =
+    "INSERT INTO users (name, email, hash_password) VALUES ($1, $2, $3) RETURNING *";
+  const { rows } = await database.query(queryString, [
+    name,
+    email,
+    hashPassword,
+  ]);
+  return rows[0];
+}
+
+export async function findUserByEmail(email) {
+  const queryString = "SELECT * FROM users WHERE email = $1";
+  const { rows } = await database.query(queryString, [email]);
+  return rows[0];
+}
