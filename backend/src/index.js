9ee39876c77a0fb897f3f5f4fdc44aa8d600a886
augolv <soninclovis@gmail.com>
2025-03-10 16:00:44 -0300

    feat: add JWT authentication middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,35 @@
 import e from "express";
 import "dotenv/config";
+import jwt from "jsonwebtoken";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = e();
 const port = process.env.PORT;
 
 app.use(e.json());
-app.use("/auth", authRoutes);
+
+const validateJWT = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(403).json({ error: "Invalid or exprired token" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server rodando em http://localhost:${port}`);
