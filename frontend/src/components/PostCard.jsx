b5969f04eeb7bfd1f28f7b3e65a7bfc6d4542f31
augolv <soninclovis@gmail.com>
2025-03-23 17:34:43 -0300

    feat: set up basic routing with react-router

---
+++ frontend/src/components/PostCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PostCard({ post }) {
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content.substring(0, 400)}...</p>
+    </div>
+  );
+}
+
+export default PostCard;
