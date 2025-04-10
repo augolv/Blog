e2303973baae8eb73cfa3577b85f8cc585b5d2e8
augolv <soninclovis@gmail.com>
2025-04-10 17:51:14 -0300

    fix: use named export to resolve import issues in api service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,3 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
-
-export default api;
+export const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
