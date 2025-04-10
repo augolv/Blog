ffafa2f4e36ba168a2a020ea401c9b6a9679ba2a
augolv <soninclovis@gmail.com>
2025-04-10 17:54:07 -0300

    fix: add cors middleware to resolve cross-origin issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import e from "express";
 import "dotenv/config";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 
@@ -7,6 +8,7 @@ const app = e();
 const port = process.env.PORT;
 
 app.use(e.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
