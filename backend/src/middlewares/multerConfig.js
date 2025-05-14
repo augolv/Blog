5d0bac408332f828353338b45a53393092bd7869
augolv <soninclovis@gmail.com>
2025-05-14 16:27:45 -0300

    feat: add cloudinary configuration and multer middleware for image uploads

---
+++ backend/src/middlewares/multerConfig.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+
+// Configuração de armazenamento em memória
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only JPEG and PNG images are allowed"), false);
+  }
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  },
+});
